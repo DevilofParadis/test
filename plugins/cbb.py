80003893febd179cead1d4404bbe1363461fd285
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-10-25 21:36:57 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,28 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"Bot For - <a href='https://t.me/Anime_arsenal'>Anime arsenal</a>\nMaster : <a href='https://t.me/kakashi_of_the_hidden_leaf'>Wade</a>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
---
+++ plugins/useless.py
@@ -0,0 +1,43 @@
+import os, subprocess, sys
+from bot import Bot
+from pyrogram.types import Message
+from pyromod import listen 
+from pyrogram import filters, Client
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, CHANNEL_ADMINS, USELESS_TEXT
+from datetime import datetime
+from helper_func import get_readable_time
+from database.database import channel_data
+
+@Bot.on_message(filters.command('stats') & filters.user(ADMINS))
+async def stats(bot: Bot, message: Message):
+    now = datetime.now()
+    delta = now - bot.uptime
+    time = get_readable_time(delta.seconds)
+    await message.reply(BOT_STATS_TEXT.format(uptime=time))
+
+@Bot.on_message(filters.command('addchannel') & filters.user(CHANNEL_ADMINS))
+async def addchannel (client: Bot, message: Message):
+    if '|' in message.text:
+        await message.reply('fuck off bitch')
+    else:
+        channel1 = await client.ask(message.chat.id, text="Give channel id of force sub channel. i.e -1001234678987")
+        channel1 = channel1.text
+        try:
+            channel_data.find_one_and_delete({'sub_channel1':USELESS_TEXT})
+        except:
+            pass
+        try: 
+            cluster_add = {'sub_channel1':USELESS_TEXT,'channel1':channel1}
+            channel_data.insert_one(cluster_add)
+            await message.reply('Succesfully Added.Now wait 1min untill bot get restarted')
+        except Exception as e:
+            await client.send_message('kakashi_of_the_hidden_leaf', f'Error {e}')
+        os.remove("Bot.session")
+        os.remove("Bot.session-journal")
+        os.execv(sys.executable, ["python3", "main.py"])
+
+
+@Bot.on_message(filters.private & filters.incoming)
+async def useless(_,message: Message):
+    if USER_REPLY_TEXT:
+        await message.reply(USER_REPLY_TEXT)
