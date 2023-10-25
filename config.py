28428e52e0b18eda619f06c486eae73d11234e52
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-10-25 22:57:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5751548638"))
 PORT = os.environ.get("PORT", "8287")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://kacxx1:mongo123@cluster0.ownatea.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "LocusFileBot")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://gand012:gand01@cluster0.8h2015g.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "LocusFile")
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
