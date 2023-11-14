ec389b786a040a61a7674906a1ec31718c6c9de6
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-11-14 16:53:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,12 +39,12 @@ class Bot(Client):
                 self.invitelink = link 
                 
         if FORCE_SUB_CHANNEL is not None:
-           try:
-               link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-               if not link:
-                   await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
-                   link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
-               self.invitelink2 = link
+            try:
+                link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                if not link:
+                    await self.export_chat_invite_link(FORCE_SUB_CHANNEL)
+                    link = (await self.get_chat(FORCE_SUB_CHANNEL)).invite_link
+                self.invitelink2 = link
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
