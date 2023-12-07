abe6ef57023be098793790792b62835479725f3d
Pandey Amit <114922080+DevilofParadis@users.noreply.github.com>
2023-12-07 22:53:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "Join Channel 1",
-                url="https://t.me/+0Kwcbzh1V0phMjg9"
+                url="https://t.me/+uqtzAv8-7vA0NDVl"
             )
         ],
         [
